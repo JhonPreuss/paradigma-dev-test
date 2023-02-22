7d10e9d16cb144a5d439afe4a6e95dd3651709a5
JhonPreuss <jonathan.preuss@redes.ufsm.br>
2023-02-21 23:59:09 -0300

    Tarefa 3

---
+++ ParadigmaTarefa3/IntList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ParadigmaTarefa3
+{
+    internal class IntList
+    {
+        public int? firstElementIndex { get; set; }
+        public int? secondElementIndex { get; set; }
+
+        public void OrganizeList(List<int> data, int sumValue)
+        {
+            try
+            {
+                if (data.Count() == 0)
+                    throw new ArgumentNullException("data");
+                int? x;
+                foreach (var item in data)
+                {
+                    var index = data.IndexOf(item);
+                    this.firstElementIndex = index;
+
+                    x = GetIndexSumList(data, index, sumValue);
+                    if (x != null)
+                        this.secondElementIndex = x.Value;
+                        break;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        //retorna o index do primeiro elemento cuja a soma seja igual a soma informada
+        private int? GetIndexSumList(List<int> listData, int index, int sumValue)
+        {
+            for(int i = 0; i < listData.Count(); i++)
+            {
+                if (index != i && listData[index] + listData[i] == sumValue)
+                    return i;
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ ParadigmaTarefa3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParadigmaTarefa3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var soma = 9;
+
+            Console.WriteLine("Soma de entrada {0}",soma);
+            List<int> arrayData = new List<int>{2, 7, 11, 15};
+            IntList listDataToHandle = new IntList();
+            listDataToHandle.OrganizeList(arrayData, soma);
+
+            if (listDataToHandle.firstElementIndex == null || listDataToHandle.secondElementIndex == null)
+                throw new Exception("Não foram encontrados índices de elementos que correspondem a soma de informada");
+
+            Console.WriteLine("{0} + {1} são os index dos números encontrados no array de entrada", listDataToHandle.firstElementIndex, listDataToHandle.secondElementIndex);
+        }
+    }
+}
